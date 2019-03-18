c402309d3bd36cebad90fd0d0613dbb901e641bb
dopeness <infinitedopeness@gmail.com>
2019-03-18 09:11:46 -0700

    hobby site

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function() {  
+  $(".food").click(function() {
+      $(this).find(".pop" ).toggle();
+  });
+
+ // console.log('hi');
+ // console.log($('.food'))
+});
+/*
+$( "#other" ).click(function() {
+  $( "#target" ).click();
+});
+*/
+   
